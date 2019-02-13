b599b3e59bc9006d88571906cc1a1d7617cd58fa
Peter-G-Stone <peter.stone.online@gmail.com>
2019-02-13 15:10:13 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
-// Code EyesOnMe Component Here
+// EyesOnMe
+
+import React, { Component } from 'react';
+
+
+export default class EyesOnMe extends Component {
+
+    focusMsg = () => {
+        console.log('Good!')
+    }
+
+    blurMsg = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    render () {
+        return (
+            <button onFocus={this.focusMsg} onBlur={this.blurMsg} />
+        )
+    }
+
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
-// Code Keypad Component Here
+// KEYPAD
+
+import React, { Component } from 'react';
+
+
+
+export default class Keypad extends Component {
+
+    logMessage = () => {
+        console.log('Entering password...')
+    }
+
+    render () {
+        return (
+            <input type="password" onKeyUp={this.logMessage} />
+        )
+    }
+
+}
